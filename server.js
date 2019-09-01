289cc151e809f1f8ed171066d36d448c5580bed6
Tushar Moraye <tusharmoraye@gmail.com>
2019-09-01 20:15:25 +0530

    path fixed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static("./dist/movie-player"));
 
-app.get("/*", function(req, res) {
+app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "/dist/movie-player/index.html"));
 });
 
